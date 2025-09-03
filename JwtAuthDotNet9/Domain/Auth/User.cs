d756f38a0816abbf491d3e43b2e2fb26f9003d36
AOS-2 <tsegaab388@gmail.com>
2025-09-03 04:39:37 +0300

    Add project files.

---
+++ JwtAuthDotNet9/Domain/Auth/User.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotNet9.Domain.Auth
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
---
+++ JwtAuthDotNet9/Domain/Dto/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotNet9.Domain.Dto
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
