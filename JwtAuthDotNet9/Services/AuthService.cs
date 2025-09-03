dcbeaffe5c1647024480730cb6b7bfb624634414
AOS-2 <tsegaab388@gmail.com>
2025-09-03 05:00:26 +0300

    Added Services

---
+++ JwtAuthDotNet9/Services/AuthService.cs
@@ -0,0 +1,51 @@
+﻿using JwtAuthDotNet9.Data;
+using JwtAuthDotNet9.Domain.Auth;
+using JwtAuthDotNet9.Domain.Dto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotNet9.Services
+{
+    public class AuthService(UserDbContext context, IConfiguration configuration) : IAuthService
+    {
+        public async Task<string?> LoginAsync(UserDto request)
+        {
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Username == request.Username);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        public async Task<User?> RegisterAsync(UserDto request)
+        {
+            if(await context.Users.AnyAsync(u => u.Username == request.Username))
+            {
+                return null;
+            }
+
+            var user = new User();
+
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ JwtAuthDotNet9/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using JwtAuthDotNet9.Domain.Auth;
+using JwtAuthDotNet9.Domain.Dto;
+
+namespace JwtAuthDotNet9.Services
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDto request);
+        Task<string?> LoginAsync(UserDto request);
+    }
+}
