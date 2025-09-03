d6cf2fbb1eb098caf1b35109462230ba24de771a
AOS-2 <tsegaab388@gmail.com>
2025-09-03 04:49:13 +0300

    DbAdded

---
+++ JwtAuthDotNet9/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using JwtAuthDotNet9.Domain.Auth;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotNet9.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
--- JwtAuthDotNet9/Program.cs
+++ JwtAuthDotNet9/Program.cs
@@ -1,8 +1,14 @@
+using JwtAuthDotNet9.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UserDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
